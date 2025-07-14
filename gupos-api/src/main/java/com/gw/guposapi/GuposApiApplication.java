60396622945a0bc21dd77c96954c225371f5585a
김건우 <gwkim@d-pub.co.kr>
2025-07-14 17:57:01 +0900

    지유 포스 서버 모듈 시작

---
+++ gupos-api/src/main/java/com/gw/guposapi/GuposApiApplication.java
@@ -0,0 +1,10 @@
+package com.gw.guposapi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuposApiApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
