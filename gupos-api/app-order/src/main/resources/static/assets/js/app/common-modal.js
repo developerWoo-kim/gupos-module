25db6c867c256631cbc74a30754eeffe50045de3
김건우 <gwkim@d-pub.co.kr>
2025-08-06 16:32:39 +0900

    app-order css deleted

--- gupos-api/app-order/src/main/resources/static/assets/js/app/common-modal.js
+++
@@ -1,8 +0,0 @@
-function addContent(el, data) {
-    $(el).children(".modal-dialog").empty();
-    $(el).children(".modal-dialog").append(data);
-}
-
-$(document).on("click", ".close-modal", function () {
-    $(this).parent().parent().parent().empty();
-})
